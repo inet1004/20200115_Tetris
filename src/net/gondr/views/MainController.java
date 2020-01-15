8cd6dd2acb721963e1c40e6d20db184196d6ae56
User <User@YD01-18>
2020-01-15 13:52:07 +0900

    20200115

---
+++ src/net/gondr/views/MainController.java
@@ -0,0 +1,20 @@
+package net.gondr.views;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import net.gondr.tetris.App;
+import net.gondr.tetris.Game;
+
+public class MainController {
+	@FXML
+	private Canvas gameCanvas;
+	
+	@FXML
+	public void initialize() {
+		System.out.println("메인 레이아웃 초기화");
+		Main.app.game = new Game(gameCanvas);
+		
+	}
+
+}
